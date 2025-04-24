52da450c2cf68c9532199f14eff4ceb1a4a18c26
JEH <koreaitjeh@gmail.com>
2025-04-24 17:47:58 +0900

    router

--- router/auth.mjs
+++ router/auth.mjs
@@ -3,6 +3,7 @@
 import express from "express";
 import * as authController from "../controller/auth.mjs";
 
+const session = require("express-session");
 const router = express.Router();
 
 // 회원가입
@@ -14,4 +15,37 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 // 로그인 유지
+router.use(
+  session({
+    secret: "!@#$%^&*()",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
+router.post("/login", (req, res) => {
+  const { userid, password } = req.body;
+  req.session.user = { userid };
+  res.send(`로그인 성공: ${userid}`);
+});
+
+router.get("/me", (req, res) => {
+  if (req.session.user) {
+    res.json(req.session.user); //session에 user가 있다면
+  } else {
+    res.status(401).send("로그인이 필요합니다.");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.send("로그아웃 되었습니다.");
+  });
+});
+
+router.listen(3000, () => {
+  console.log("서버 실행 중");
+});
+
 export default router;
